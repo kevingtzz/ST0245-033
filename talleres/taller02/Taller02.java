f7114d67f89111891f063cbbeaf568551ab645e4
Kevin Gutierrez <31072835+kgutierreg@users.noreply.github.com>
2018-01-26 14:45:25 -0500

    Update Taller02.java

--- talleres/taller02/Taller02.java
+++ talleres/taller02/Taller02.java
@@ -25,7 +25,7 @@ class Taller02 {
     if (s.length() == 0) {
       System.out.println(base);
     }else{ combinations(base + s.charAt(0), s.substring(1));
-	combinations(base, s.substring(1));
+	   combinations(base, s.substring(1));
          } 
     }
 
