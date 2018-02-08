52f6ebf7226bc9180662206c85a8429a07a59518
Kevin Gutierrez <kgutierreg@eafit.edu.co>
2018-02-08 04:24:42 -0500

    punto04

--- talleres/taller04/taller04.py
+++ talleres/taller04/taller04.py
@@ -2,7 +2,7 @@
 import random
 
 def array_generator(len):
-    """Generador de arrays"""
+    """List generator"""
     array = [0] * len
     for i in range(len):
         array[i] = random.randrange(0,100)
@@ -10,7 +10,7 @@ def array_generator(len):
     return array
 
 def array_sum(array, sum = 0):
-    """Suma elementos de el arreglo que reciba"""
+    """Add the elements in the list"""
     for element in array:
         sum += element
 
@@ -21,3 +21,11 @@ def multiplication_tables(n):
         for j in range(1,n + 1):
             print str(i) + " * " + str(j) + " = " + str(i*j)
         print "--------------------"
+
+def insertion_sort(list):
+    for index in range(len(list)):
+        for j in range(index,0,-1):
+            if list[j-1] > list[j]:
+                tmp = list[j]
+                list[j] = list[j-1]
+                list[j-1] = tmp
