8c90bee75343868f153e1f9b393df62e9e7dff25
Kevin Gutierrez <kgutierreg@eafit.edu.co>
2018-02-09 09:22:29 -0500

    Correción taller 01 en python

--- talleres/taller01/taller01.py
+++ talleres/taller01/taller01.py
@@ -3,7 +3,8 @@ import math
 
 class Counter():
     """counter."""
-    x = 0;
+    def __init__(self):
+        self.__x = 0
 
     def increase(self):
         self.x = self.x + 1
@@ -13,10 +14,34 @@ class Counter():
 
     def toString(self):
         print str(self.x)
-        
-counter = Counter()
+#
+#counter = Counter()
+#
+#con = range(10)
+#for i in con:
+#    counter.increase()
+#    counter.toString()
+#
+class Punto2D():
+    """Representacion de punto en 2 dimenciones"""
 
-con = range(10)
-for i in con:
-    counter.increase()
-    counter.toString()
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_x(self):
+        return x
+
+    def get_y(self):
+        return y
+
+    def radio_polar(self):
+        return math.sqrt((x * x) + (y * y)
+
+    def angulo_polar(self):
+        return math.atan2(x / y)
+
+    def dist_euclidiana(self, Punto2D other):
+        dx = self.x - other.get_x()
+        dy = self.y - other.get_y()
+        return math.sqrt((dx * dx) + (dy * dy))
