50628d04ec57c927216e1a4a2a0c33805973ea22
kevin Gutierrez Gomez <kgutierreg@eafit.edu.co>
2018-03-12 14:14:45 -0500

    final com

--- talleres/taller08/taller08.java
+++ talleres/taller08/taller08.java
@@ -25,10 +25,46 @@ public class taller08 {
       queue.add("Juan");
       queue.add("Maria");
       queue.add("Pedro");
-
-      for (String cliente : queue) {
-        System.out.println(cliente + " fue atendid@.");
+      int len = queue.size();
+      for (int i = 0; i < len; ++i) {
+        System.out.println(queue.remove() + " fue atendid@.");
       }
 
+      //::::::::::::::::::::::::::::::Notacion Polaca::::::::::::::::::::::::::::\\
+
+
+
+    Stack<Integer> stacks = new Stack<>();
+    String string = "3 10 5 + *";
+    String buff[] = string.split(" ");
+
+    for (int i = 0; i < buff.length; i++) {
+        String current = buff[i];
+        if (!taller08.isOperator(current)) {
+            stacks.push(Integer.parseInt(current));
+        } else {
+            int fact2 = stacks.pop();
+            int fact1 = stacks.pop();
+            stacks.push(taller08.oper(current, fact1, fact2));
+        }
+    }
+
+    System.out.println(stacks.pop());
+  }
+
+  public static int oper(String operator, int fact1, int fact2) {
+    switch (operator) {
+      case "+": return fact1 + fact2;
+      case "-": return fact1 - fact2;
+      case "*": return fact1 * fact2;
+      default: return fact1 / fact2;
+    }
+  }
+
+    public static boolean isOperator(String str) {
+      return str.equals("+")
+      || str.equals("-")
+      || str.equals("*")
+      || str.equals("/");
     }
 }
