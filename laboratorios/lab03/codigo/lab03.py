853a9d3c51a22e72d6c3c3a7d6d16352c9d0177e
kevin Gutierrez Gomez <kgutierreg@eafit.edu.co>
2018-03-18 00:26:42 -0500

    code

--- laboratorios/lab03/codigo/lab03.py
+++ laboratorios/lab03/codigo/lab03.py
@@ -43,7 +43,6 @@ def smartInsert(list, data):
         list.append(data)
 
 def get_order(client, fridges):
-<<<<<<< HEAD
     """
     This function returns a list with the refrigerators sales
     that correspond to each client according to the order of the order;
@@ -66,45 +65,3 @@ def get_order(client, fridges):
                 frid.append(fridges.pop())
         sales.append((order[0],frid))
     return sales
-=======
-    sales = []
-    for order in client:
-        frid  = []
-        for fridge in range(0,order[1]):
-            if fridges.empty():
-                frid.append("pending order")
-            else:
-                frid.append(fridges.pop())
-        sales.append((order[0],frid))
-    return sales
-
-
-
-fridges = Stack()
-client = []
-
-for i in range(21):
-    fridges.add((i, "Esta es la nevera No." + str(i)))
-
-client.append(["tienda de pedro", 5])
-client.append(["tienda de juan", 2])
-client.append(["tienda de arnulfo",4])
-client.append(["tienda de mateo", 7])
-client.append(["tienda de jose", 1])
-client.append(["tienda de carolina",1])
-client.append(["tienda de va der waals",2])
-client.append(["tienda de hugo", 2])
-client.append(["tienda de caterine",1])
-
-print (get_order(client, fridges))
-
-"""
-list1 = [10,20,5,3,20,10]
-list2 = [10,2,4,8]
-list3 = [10,2,5,2,11]
-
-balance(list1)
-balance(list2)
-balance(list3)
-"""
->>>>>>> b772efccec6b000445cb35a18ccd028a62928587
